0e11fdbedf8c661c31f409e42885ccc77a7b4aeb
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2023-10-12 11:18:00 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" {
-  description = "The ID of the project where the resources will be created."
-}
-
-variable "region" {
-  description = "The region where the resources will be created."
-  default     = "us-central1"
-}
